96ef018d94fa10e58e9e31729d22ea5f9a8169b6
IDWright451 <81587201+IDWright451@users.noreply.github.com>
2024-02-24 19:20:59 -0500

    Added key and key script
    Added key and key script

---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour
+{
+    public GameObject gateCollider;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {   
+            //disabling 2D Box Collider on invisible gate
+            Debug.Log("Suit Picked Up");
+            gateCollider.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
